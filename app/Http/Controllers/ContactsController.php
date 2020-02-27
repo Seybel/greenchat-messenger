be034b6bdffb4b4e4d1438cbdbdb8d5d0763bde7
Seybel <seyibello31@gmail.com>
2020-02-27 13:02:05 +0100

    updated contact controller

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -12,14 +12,14 @@ class ContactsController extends Controller
     public function get()
     {
         // get all users except the authenticated one
-        $contacts = User::where('id', '!=', auth()->id())->get();
+        $contacts = User::where("id", '!=', auth()->id())->get();
 
         // get a collection of items where sender_id is the user who sent us a message
         // and messages_count is the number of unread messages we have from him
-        $unreadIds = Message::select(\DB::raw('`from` as sender_id, count(`from`) as messages_count'))
-            ->where('to', auth()->id())
-            ->where('read', false)
-            ->groupBy('from')
+        $unreadIds = Message::select(\DB::raw('"from" as sender_id, count("from") as messages_count'))
+            ->where("to", auth()->id())
+            ->where("read", false)
+            ->groupBy("from")
             ->get();
 
         // add an unread key to each contact with the count of unread messages
@@ -38,15 +38,15 @@ class ContactsController extends Controller
     public function getMessagesFor($id)
     {
         // mark all messages with the selected contact as read
-        Message::where('from', $id)->where('to', auth()->id())->update(['read' => true]);
+        Message::where("from", $id)->where("to", auth()->id())->update(["read" => true]);
 
         // get all messages between the authenticated user and the selected user
         $messages = Message::where(function($q) use ($id) {
-            $q->where('from', auth()->id());
-            $q->where('to', $id);
+            $q->where("from", auth()->id());
+            $q->where("to", $id);
         })->orWhere(function($q) use ($id) {
-            $q->where('from', $id);
-            $q->where('to', auth()->id());
+            $q->where("from", $id);
+            $q->where("to", auth()->id());
         })
         ->get();
 
@@ -56,9 +56,9 @@ class ContactsController extends Controller
     public function send(Request $request)
     {
         $message = Message::create([
-            'from' => auth()->id(),
-            'to' => $request->contact_id,
-            'text' => $request->text
+            "from" => auth()->id(),
+            "to" => $request->contact_id,
+            "text" => $request->text
         ]);
 
         broadcast(new NewMessage($message));
