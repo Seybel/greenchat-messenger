37f93e4df8c3da5d91ba1baaccfa6f0fe5e0d6b5
Seybel <seyibello31@gmail.com>
2020-02-27 15:10:56 +0100

    updated blade files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Greenchat
                 </div>
 
                 <div class="links">
